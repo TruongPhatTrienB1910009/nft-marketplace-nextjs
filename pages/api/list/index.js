ccf24a1e0193bcfc695b4d903cd49f8d974ba5af
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-05-18 09:39:02 +0700

    Update data

--- pages/api/list/index.js
+++ pages/api/list/index.js
@@ -1,20 +1,20 @@
 const data = [
     {
         id: 0,
-        name: 'Silver Box #0',
-        image: '/images/box0.png',
+        name: 'Silver Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box0.png',
         description: 'CORE DOGE NFT 1'
     },
     {
         id: 1,
-        name: 'Gold Box #1',
-        image: '/images/box1.png',
+        name: 'Gold Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box1.png',
         description: 'CORE DOGE NFT 2'
     },
     {
         id: 2,
-        name: 'Ruby Box #2',
-        image: '/images/box2.png',
+        name: 'Ruby Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box2.png',
         description: 'CORE DOGE NFT 3'
     }
 ]
--- pages/api/nft/[pid].js
+++ pages/api/nft/[pid].js
@@ -1,7 +1,22 @@
 const data = [
-    { "a": 1 },
-    { "b": 2 },
-    { "c": 3 }
+    {
+        id: 0,
+        name: 'Silver Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box0.png',
+        description: 'CORE DOGE NFT 1'
+    },
+    {
+        id: 1,
+        name: 'Gold Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box1.png',
+        description: 'CORE DOGE NFT 2'
+    },
+    {
+        id: 2,
+        name: 'Ruby Box',
+        image: 'https://nft-marketplace-nextjs-rho.vercel.app/images/box2.png',
+        description: 'CORE DOGE NFT 3'
+    }
 ]
 
 export default function handler(req, res) {
