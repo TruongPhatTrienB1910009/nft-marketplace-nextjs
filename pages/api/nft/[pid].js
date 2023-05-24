64a2713e55f99b9ac6ff13a2432063e0a1ee7e38
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-05-24 11:36:01 +0700

    update data

--- pages/api/nft/[pid].js
+++ pages/api/nft/[pid].js
@@ -3,28 +3,28 @@ const data = [
     {
         id: 0,
         name: 'Silver Box',
-        image: `https://nft-marketplace-nextjs-roan.vercel.app/images/box0.png`,
-        description: 'CORE DOGE NFT 1'
+        image: 'https://nft-marketplace-nextjs-roan.vercel.app/images/box0.png',
+        desc: 'CORE DOGE NFT 1'
     },
     {
         id: 1,
         name: 'Gold Box',
         image: 'https://nft-marketplace-nextjs-roan.vercel.app/images/box1.png',
-        description: 'CORE DOGE NFT 2'
+        desc: 'CORE DOGE NFT 2'
     },
     {
         id: 2,
         name: 'Ruby Box',
         image: 'https://nft-marketplace-nextjs-roan.vercel.app/images/box2.png',
-        description: 'CORE DOGE NFT 3'
+        desc: 'CORE DOGE NFT 3'
     }
 ]
-
-
 
 export default async function handler(req, res) {
     const { pid } = req.query;
     const Type = await getNftType(pid);
-    console.log(location.hostname)
-    // res.send(data[Type]);
+    data[Type].nftType = Number(Type);
+    data[Type].name += ` #${pid}`
+
+    res.send(data[Type]);
 }
