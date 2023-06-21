e9a9b5c496a655bb7ba295a5a909376f2a4565e3
KhoaVo0512 <94926137+KhoaVo0512@users.noreply.github.com>
2023-06-21 15:03:00 +0700

    Update fetDataNft.js

--- src/fetData/fetDataNft.js
+++ src/fetData/fetDataNft.js
@@ -1,7 +1,7 @@
 const abi = require("../abi/marketplace.json");
 const Web3 = require("web3");
 const web3 = new Web3('https://rpc-core.icecreamswap.com');
-const contractAddress = '0xC24899C146835c6566629652152eae44210A96F6';
+const contractAddress = '0x6EFCA034E8f6745858976d9b4A41bDc60D6691e6';
 
 const contract = new web3.eth.Contract(abi, contractAddress);
 
